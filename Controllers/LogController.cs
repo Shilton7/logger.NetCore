c2d4276d58d100c83d6e78dccfcb5aea0b92dfae
SHILTON PEREIRA SANTIAGO - 900000968 <shilton.santiago@animaeducacao.com.br>
2021-06-19 14:15:14 -0300

    Installing and configuring kisslog

---
+++ Controllers/LogController.cs
@@ -0,0 +1,64 @@
+﻿using KissLog;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace SavingLogs.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class LogController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public LogController(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("trace")]
+        public void Trace()
+        {
+            _logger.Trace("Trace Here");
+        }
+
+        [HttpGet("debug")]
+        public void Debug()
+        {
+            _logger.Debug("Debug Here");
+        }
+
+        [HttpGet("warn")]
+        public void Warn()
+        {
+            _logger.Warn("Warn Here");
+        }
+
+        [HttpGet("error")]
+        public void Error()
+        {
+            _logger.Error("Error Here");
+        }
+
+        [HttpGet("exception")]
+        public void Exception()
+        {
+            try
+            {
+                throw new Exception("Exception Here");
+            }
+            catch(Exception e)
+            {
+                _logger.Error(e);
+            }
+            
+        }
+
+        [HttpGet("exception-with-stack-trace")]
+        public void ExceptionWithStackTrace()
+        {
+            throw new Exception("Exception Here");
+
+        }
+
+    }
+}
